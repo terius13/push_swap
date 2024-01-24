c8a9616a87a7c53f3c4d5187172a8c60017f8e83
Timberly <teriusng13@gmail.com>
2024-01-24 17:28:01 +0800

    added struct

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,11 +6,17 @@
 /*   By: ting <ting@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 11:25:50 by ting              #+#    #+#             */
-/*   Updated: 2024/01/22 11:26:36 by ting             ###   ########.fr       */
+/*   Updated: 2024/01/24 17:27:47 by ting             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
+
+typedef struct	s_list
+{
+	int	data;
+	struct s_list	*next;
+}	t_stack;
 
 #endif
