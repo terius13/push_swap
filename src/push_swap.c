679743b136d329dcb6696645f7b6180c55c9d4a4
Timberly <teriusng13@gmail.com>
2024-01-29 17:15:35 +0800

    added struct

---
+++ src/push_swap.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ting <ting@student.42singapore.sg>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/29 12:54:01 by ting              #+#    #+#             */
+/*   Updated: 2024/01/29 13:30:36 by ting             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+//need to declare the struct for stack a and b
+
+int	main(int argc, char **argv)
+{
+	t_stack	stack_a;
+	t_stack stack_b;
+
+	if (argv < 2)
+	{
+	
+	}
+	else if (argv > 2)
+	{
+	
+	}
+	else if (argv == 2)
+}
