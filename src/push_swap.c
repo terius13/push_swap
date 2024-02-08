315f5974e54ddc44776f59439d259464e7e88807
Timberly Ng <ting@c1r8s6.42singapore.sg>
2024-02-08 20:38:49 +0800

    erase push_swap.c

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: ting <ting@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 12:54:01 by ting              #+#    #+#             */
-/*   Updated: 2024/01/29 13:30:36 by ting             ###   ########.fr       */
+/*   Updated: 2024/02/08 20:34:04 by ting             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,5 @@
 
 int	main(int argc, char **argv)
 {
-	t_stack	stack_a;
-	t_stack stack_b;
 
-	if (argv < 2)
-	{
-	
-	}
-	else if (argv > 2)
-	{
-	
-	}
-	else if (argv == 2)
 }
