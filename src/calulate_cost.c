671723886aa05cb70ba78f7fa55bb65f760361f5
Timberly <teriusng13@gmail.com>
2024-03-07 14:10:20 +0800

    fix it, target pos works now

---
+++ src/calulate_cost.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   calulate_cost.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ting <ting@student.42singapore.sg>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/07 12:44:53 by ting              #+#    #+#             */
+/*   Updated: 2024/03/07 14:10:04 by ting             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/push_swap.h"
+
+//i will update the cost to stack_b element directly
+void	cal_cost_a(t_stack **stack_a, t_stack **stack_b)
+{
+	
+}
+
+void	cal_cost_b(t_stack **stack_a, t_stack **stack_b)
+{
+
+}
+
